d60eb8ed14ee3591104c6393b2c8626862c6060c
ziwei-zhu <zzhu39@illinois.edu>
2019-09-24 17:44:03 -0500

    parameter-override

--- script/aws.sh
+++ script/aws.sh
@@ -4,4 +4,4 @@
 export PATH=/usr/local/bin:$PATH
 export BRANCH=$GIT_BRANCH
 
-aws cloudformation deploy --stack-name ${BRANCH}-test-wendy --template-file cloudformation/${BRANCH}/template.json --parameters ParameterKey=BucketNameParameter,ParameterValue=${BRANCH} --profile default
+aws cloudformation deploy --stack-name ${BRANCH}-test-wendy --template-file cloudformation/${BRANCH}/template.json --parameters-overrides ParameterKey=BucketNameParameter,ParameterValue=${BRANCH} --profile default
